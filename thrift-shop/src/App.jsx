814e6254e9c54f167041bb8aea25c722a55095d1
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-03-09 14:20:51 +0200

    Add a footer and styles to it

--- thrift-shop/src/App.jsx
+++ thrift-shop/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/main/footer/Footer'
 import Header from './components/main/header/Header'
 
 function App() {
@@ -6,6 +7,12 @@ function App() {
     return (
         <>
             <Header />
+
+            <main>
+                <h1>Main content</h1>
+            </main>
+
+            <Footer />
         </>
     )
 }
---
+++ thrift-shop/src/components/main/footer/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer><p>Created by <strong>Viktor Stoichev</strong> &copy;</p></footer>
+    );
+}
