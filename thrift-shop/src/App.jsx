fd3aac9f9007bcd51092dcae19936e6fff6ee930
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-03-09 14:36:33 +0200

    Add routing for auth components

--- thrift-shop/src/App.jsx
+++ thrift-shop/src/App.jsx
@@ -1,19 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from './components/main/footer/Footer'
 import Header from './components/main/header/Header'
+import Profile from './components/auth/Profile';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
 
 function App() {
 
     return (
-        <>
+        <Router>
             <Header />
 
             <main>
-                <h1>Main content</h1>
+                <Routes>
+                    <Route path="/" element={<h1>Main content</h1>} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                </Routes>
             </main>
 
             <Footer />
-        </>
+        </Router>
     )
 }
 
--- thrift-shop/src/components/main/header/Header.jsx
+++ thrift-shop/src/components/main/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 export default function Header() {
@@ -7,19 +8,19 @@ export default function Header() {
             <h1>Thrift Shop</h1>
             <nav className='main-links'>
                 <ul>
-                    <li><a href=""><i className="fa-solid fa-house"></i></a></li>
-                    <li><a href=""><i className="fa-solid fa-heart"></i></a></li>
-                    <li><a href=""><i className="fa-solid fa-user"></i></a></li>
-                    <li><a href="">Products</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href=""><i className="fa-solid fa-phone"></i></a></li>
+                    <li><Link to="/"><i className="fa-solid fa-house"></i></Link></li>
+                    <li><Link to=""><i className="fa-solid fa-heart"></i></Link></li>
+                    <li><Link to="/profile"><i className="fa-solid fa-user"></i></Link></li>
+                    <li><Link to="">Products</Link></li>
+                    <li><Link to="">About</Link></li>
+                    <li><Link to=""><i className="fa-solid fa-phone"></i></Link></li>
                 </ul>
             </nav>
             <nav className='user-links'>
                 <ul>
-                    <li><a href="">Login</a></li>
-                    <li><a href="">Register</a></li>
-                    <li><a href="">Logout</a></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                    <li><Link to="logout">Logout</Link></li>
                 </ul>
             </nav>
         </header>
