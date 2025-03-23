794f9e049b3aea2d0e6b7d3e8414f4b024278cea
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-03-23 12:36:13 +0200

    Add confirm modal to selling phone component

--- client/src/components/items/sell-phone/SellPhone.jsx
+++ client/src/components/items/sell-phone/SellPhone.jsx
@@ -2,42 +2,63 @@ import { useErrorHandler } from "../../../errors/handleError";
 import { useCreate } from "../../../hook-api/UseCreate";
 import { useState } from "react";
 import "./SellPhone.css";
+import ConfirmModal from "../../main/confirm-modal/ConfirmModal";
 
 export default function AddItem() {
     const { errors, visibleErrors, handlePhoneDataError } = useErrorHandler();
     const { createAction } = useCreate();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [formData, setFormData] = useState(null);
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+        setFormData(new FormData(e.target));
+        setIsModalOpen(true);
+    }
 
     return (
-        <div className="sell-phone-container">
-            <h2 className="sell-phone-title">Sell Phone</h2>
-            <form className="sell-phone-form" action={createAction}>
-                <label>Title:</label>
-                <input type="text" name="title" required onBlur={handlePhoneDataError} />
-                {errors.title && <span className={`error-text ${visibleErrors.title ? "show" : ""}`}>{errors.title}</span>}
+        <>
+            <ConfirmModal
+                isOpen={isModalOpen}
+                onClose={() => setIsModalOpen(false)}
+                onConfirm={() => {
+                    if (formData) createAction(formData);
+                    setIsModalOpen(false);
+                }}
+                title={"Confirm Selling this Phone"}
+                message={"Are you sure you want to sell this phone?"}
+            />
+            <div className="sell-phone-container">
+                <h2 className="sell-phone-title">Sell Phone</h2>
+                <form className="sell-phone-form" onSubmit={onFormSubmit}>
+                    <label>Title:</label>
+                    <input type="text" name="title" required onBlur={handlePhoneDataError} />
+                    {errors.title && <span className={`error-text ${visibleErrors.title ? "show" : ""}`}>{errors.title}</span>}
 
-                <label>Image URL:</label>
-                <input type="text" name="imageUrl" required onBlur={handlePhoneDataError} />
-                {errors.imageUrl && <span className={`error-text ${visibleErrors.imageUrl ? "show" : ""}`}>{errors.imageUrl}</span>}
+                    <label>Image URL:</label>
+                    <input type="text" name="imageUrl" required onBlur={handlePhoneDataError} />
+                    {errors.imageUrl && <span className={`error-text ${visibleErrors.imageUrl ? "show" : ""}`}>{errors.imageUrl}</span>}
 
-                <label>Price:</label>
-                <input type="number" name="price" required onBlur={handlePhoneDataError} />
-                {errors.price && <span className={`error-text ${visibleErrors.price ? "show" : ""}`}>{errors.price}</span>}
+                    <label>Price:</label>
+                    <input type="number" name="price" required onBlur={handlePhoneDataError} />
+                    {errors.price && <span className={`error-text ${visibleErrors.price ? "show" : ""}`}>{errors.price}</span>}
 
-                <label>Description:</label>
-                <textarea name="description" required onBlur={handlePhoneDataError}></textarea>
-                {errors.description && <span className={`error-text ${visibleErrors.description ? "show" : ""}`}>{errors.description}</span>}
+                    <label>Description:</label>
+                    <textarea name="description" required onBlur={handlePhoneDataError}></textarea>
+                    {errors.description && <span className={`error-text ${visibleErrors.description ? "show" : ""}`}>{errors.description}</span>}
 
-                <div className="actions">
-                    <button 
-                        type="submit" 
-                        className="submit-button" 
-                        style={{ backgroundColor: Object.values(errors).some(Boolean) ? "grey" : "" }} 
-                        disabled={Object.values(errors).some(Boolean)}
-                    >
-                        Submit
-                    </button>
-                </div>
-            </form>
-        </div>
+                    <div className="actions">
+                        <button
+                            type="submit"
+                            className="submit-button"
+                            style={{ backgroundColor: Object.values(errors).some(Boolean) ? "grey" : "" }}
+                            disabled={Object.values(errors).some(Boolean)}
+                        >
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </>
     );
 }
